8164704b3d9ecd9474c49aff64d4c78c442908d3
tanmaytanmay <fadnavis_tanmay@yahoo.co.in>
2013-12-24 01:27:17 +0530

    adding basic UI

---
+++ login.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<title>Login</title>
+	<meta name="description" content="starplus" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/bootstrap-responsive.min.css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+
+<div class="col-md-2" style="margin-left:450px;margin-top:300px;">
+	<a href="#" id="facebook"><img src="images/fblogin.png" height="60px" width="200px"/></a>
+</div>
+<div class="col-md-2"  style="margin-top:297px;">
+	<a href="#" id="facebook"><img src="images/linkedinlogin.png" height="65px" width="200px"/></a>
+</div>
+
+
+
+<footer id="footer" style="position:absolute;bottom:0;height:60px;width:100%;text-align:right;">
+
+	<section>With love<img src="images/logo.png" height="60px" width="120px" />
+	</section>
+
+</footer>
+
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
